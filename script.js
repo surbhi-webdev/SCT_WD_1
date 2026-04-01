68e076e66fb0a1a65a3bd1265e12d0950d927c21
surbhi-webdev <surbhiharde@gmail.com>
2026-04-02 00:01:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onscroll = function () {
+    let navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+};
+
